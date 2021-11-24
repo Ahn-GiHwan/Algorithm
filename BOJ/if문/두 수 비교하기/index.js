daa345cd6bd51a158c541e73dc48c5b388fb6f12
Ahn-Gihwan <ahngh0113@naver.com>
2021-11-24 12:47:27 +0900

    1330 두 수 비교하기

--- BOJ/if문/두 수 비교하기/index.js
+++ BOJ/if문/두 수 비교하기/index.js
@@ -1,7 +1,7 @@
 // readline 모듈보다는 fs를 이용해 파일 전체를 읽어 들여 처리하기
 let fs = require("fs");
-// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
-let input = fs.readFileSync("input.txt").toString().split("\n");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+// let input = fs.readFileSync("input.txt").toString().split("\n");
 
 data = input[0].split(" ");
 
