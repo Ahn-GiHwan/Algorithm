058c4c3cc91aab02252fedfc70ac133268925c3f
Ahn-Gihwan <ahngh0113@naver.com>
2021-12-14 00:22:09 +0900

    binarySearch

--- BOJ/binarySearch/binarySearch.js
+++ BOJ/binarySearch/binarySearch.js
@@ -13,3 +13,13 @@ function binarySearch(arr, target, start, end) {
   }
   return -1;
 }
+
+function binarySearch(arr, target, start, end) {
+  while (start <= end) {
+    let mid = parseInt((start + end) / 2);
+    if (arr[mid] == target) return mid;
+    else if (arr[mid] > target) end = mid - 1;
+    else start = mid + 1;
+  }
+  return -1;
+}
