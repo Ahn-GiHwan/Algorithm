e0c838bfd2b0a809abef09414b18955717373d5c
Ahn-Gihwan <ahngh0113@naver.com>
2021-12-15 22:36:45 +0900

    입력받기방법

---
+++ BOJ/index.js
@@ -0,0 +1,39 @@
+// readline 한 줄 입력
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", function (line) {
+  console.log(line);
+
+  rl.close();
+}).on("close", function () {
+  process.exit();
+});
+
+// readline 여러줄 입력
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  console.log(input);
+  process.exit();
+});
+
+// fs 사용
+let fs = require("fs");
+// let input = fs.readFileSync('/dev/stdin').toString()
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+// let input = fs.readFileSync('input.txt').toString()
+let input = fs.readFileSync("input.txt").toString().split("\n");
