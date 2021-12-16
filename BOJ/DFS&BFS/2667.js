6db403e0f772c5b3a21537ba35a6ecc59afc8a6c
Ahn-Gihwan <ahngh0113@naver.com>
2021-12-16 21:42:35 +0900

    DFS

--- BOJ/DFS&BFS/2667.js
+++ BOJ/DFS&BFS/2667.js
@@ -7,36 +7,53 @@ let input = fs.readFileSync("input.txt").toString().split("\n");
 
 // ==================================================
 let len = Number(input[0]);
+
 let data = [];
 
-for (let i = 0; i < len; i++) {
-  const info = input[i + 1].split("").map(Number);
+for (let i = 1; i <= len; i++) {
+  const info = input[i].split("").map(Number);
   data.push(info);
 }
 
 function dfs(i, j) {
   if (i >= len || i < 0 || j >= len || j < 0) return 0;
-  if (data[i][j] >= 1) {
-    data[i][j] = -1;
-    let result = 1; // 덩어리의
-    result += dfs(i - 1, j);
-    result += dfs(i, j - 1);
-    result += dfs(i + 1, j);
-    result += dfs(i, j + 1);
-    return result;
+  if (data[i][j] === 1) {
+    data[i][j] = 0;
+    let count = 1;
+    count += dfs(i, j + 1);
+    count += dfs(i, j - 1);
+    count += dfs(i + 1, j);
+    count += dfs(i - 1, j);
+    return count;
   }
   return 0;
 }
 
-let result = []; // 덩어리(단지)를 넣어주는 배열, 배열의 개수가 단지의 개수
+let result = [];
 
 for (let i = 0; i < len; i++) {
   for (let j = 0; j < len; j++) {
-    let current = dfs(i, j);
-    if (current > 0) result.push(current);
+    const output = dfs(i, j);
+    if (output > 0) result.push(output);
   }
 }
 
 result = result.sort((a, b) => a - b);
 
 console.log(result.length + "\n" + result.join("\n"));
+
+/*
+7
+0110100
+0110101
+1110101
+0000111
+0100000
+0111110
+0111000
+================
+3
+7
+8
+9
+ */
